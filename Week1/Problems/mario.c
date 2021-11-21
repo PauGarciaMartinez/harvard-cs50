f0912726536726e80e443415bf18e8077e4a3569
Pau Garcia Martínez <73617104+PauGarciaMartinez@users.noreply.github.com>
2021-11-21 18:47:31 +0100

    Solution to mario exercise

--- Week1/Problems/mario.c
+++ Week1/Problems/mario.c
@@ -1,1 +1,35 @@
+#include <cs50.h>
+#include <stdio.h>
 
+
+int main (void)
+{
+    int pyramidHeight;
+
+    do
+    {
+        pyramidHeight = get_int("Set a height between 1 and 8, inclusive: ");
+    }
+    while (pyramidHeight < 1 | pyramidHeight > 8);
+
+    for (int i = 0; i < pyramidHeight; i++)
+    {
+        for (int k = pyramidHeight - 1; k > i; k--)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
