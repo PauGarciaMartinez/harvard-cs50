8adfc8744b1cbe0a0c3bd6a2d92033539ddaf4fe
Pau Garcia Martínez <73617104+PauGarciaMartinez@users.noreply.github.com>
2021-12-15 22:45:42 +0100

    Finish add_pairs function implementation

--- Week3/Problems/tideman.c
+++ Week3/Problems/tideman.c
@@ -126,7 +126,25 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            if (preferences[i] > preferences[j])
+            {
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                pair_count++;
+            }
+
+            if (preferences[ranks[i]] < preferences[ranks[j]])
+            {
+                pairs[pair_count].winner = j;
+                pairs[pair_count].loser = i;
+                pair_count++;
+            }
+        }
+    }
     return;
 }
 
