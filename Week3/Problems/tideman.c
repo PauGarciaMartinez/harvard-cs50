67efa1ddbcf3c6faf2397bf1176aab14a1f927f0
Pau Garcia Martínez <73617104+PauGarciaMartinez@users.noreply.github.com>
2021-12-16 12:49:54 +0100

    Solution to tideman exercise

--- Week3/Problems/tideman.c
+++ Week3/Problems/tideman.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max number of candidates
 #define MAX 9
@@ -104,7 +105,7 @@ bool vote(int rank, string name, int ranks[])
     {
         if (strcmp(candidates[i], name) == 0)
         {
-            ranks[rank] = name;
+            ranks[rank] = i;
             return true;
         }
     }
@@ -131,14 +132,13 @@ void add_pairs(void)
     {
         for (int j = i + 1; j < candidate_count; j++)
         {
-            if (preferences[i] > preferences[j])
+            if (preferences[i][j] > preferences[j][i])
             {
                 pairs[pair_count].winner = i;
                 pairs[pair_count].loser = j;
                 pair_count++;
             }
-
-            if (preferences[ranks[i]] < preferences[ranks[j]])
+            else if (preferences[i][j] < preferences[j][i])
             {
                 pairs[pair_count].winner = j;
                 pairs[pair_count].loser = i;
