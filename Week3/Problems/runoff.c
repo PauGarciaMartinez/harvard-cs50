a72e9063478f4a04dd96105164c48c628eba37c8
Pau Garcia Martínez <73617104+PauGarciaMartinez@users.noreply.github.com>
2021-12-17 12:31:42 +0100

    Solution to runoff exercise

--- Week3/Problems/runoff.c
+++ Week3/Problems/runoff.c
@@ -1,6 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <string
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -159,12 +159,13 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < voter_count; i++)
     {
-        if (candidates[i].votes > (voter_count / 2))
+        int half_vote = voter_count/2;
+        if (candidates[i].votes > half_vote)
         {
-            printf("%s", candidates[i].name);
-            return true;
+           printf("%s\n", candidates[i].name);
+           return true;
         }
     }
     return false;
@@ -177,19 +178,25 @@ int find_min(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (!candidates[i].eliminated && candidates[i].votes < min_votes) )
+        if (!candidates[i].eliminated && candidates[i].votes < min_votes)
         {
             min_votes = candidates[i].votes;
         }
     }
-    return 0;
+    return min_votes;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
-    return false;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > min)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
@@ -197,7 +204,7 @@ void eliminate(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes == min) )
+        if (candidates[i].votes == min)
         {
             candidates[i].eliminated = true;
         }
